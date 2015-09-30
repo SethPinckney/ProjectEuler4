c3f9b0896e0b9fbd8a265e12914cedaf38d21390
Daniel Wintrich <danielwintrich@Daniels-MacBook-Pro.local>
2015-09-30 16:26:58 -0400

    Working with too much output.

--- ProjectEuler4.java
+++ ProjectEuler4.java
@@ -1,36 +1,39 @@
 public class ProjectEuler4 {
 
 	public static void main (String [] args) {
-		
-		//int palin1
-		
-		for (int palin1=10; palin1 <= 99; palin1++){
+		int largestanswer = 0;
+		for (int palin1=100; palin1 <= 999; palin1++){
 			
-			for (int palinb=10; palinb <=99; palinb++){
+			for (int palinb=100; palinb <=999; palinb++){
 					
 				int answer = palin1 * palinb;
-				
-				//for (int i = 0; i <= 4; i++)
+				int saveanswer = answer;
+				int digit1 = answer % 10;
+				answer = answer / 10;
 					
-					int digit1 = answer % 10;
-					answer = answer / 10;
+				int digit2 = answer % 10;
+				answer = answer / 10;
 					
-					int digit2 = answer % 10;
-					answer = answer / 10;
+				int digit3 = answer % 10;
+				answer = answer / 10;
 					
-					int digit3 = answer % 10;
-					answer = answer / 10;
+				int digit4 = answer % 10;
+				answer = answer / 10;
 					
-					int digit4 = answer % 10;
-					answer = answer / 10;
+				int digit5 = answer % 10;
+				answer = answer / 10;
 					
+				int digit6 = answer % 10;
+				answer = answer / 10;
 					
-					
-					System.out.println("__________________________________");
-					
-					System.out.println("  "+digit4+" "+digit3+" "+digit2+" "+digit1);
-					
-				System.out.println(answer);
+				if (digit6 == digit1 && digit5 == digit2 && digit3 == digit4){
+					if (saveanswer > largestanswer) {
+						System.out.println("__________________________________");
+						System.out.println(digit6+""+digit5+""+digit4+""+digit3+""+digit2+""+digit1);
+						System.out.println(palin1+ " X "+palinb);
+						largestanswer = saveanswer;
+					}	
+				}	
 					
 			}
 				
